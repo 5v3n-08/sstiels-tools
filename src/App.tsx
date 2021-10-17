45817c6214bd9c25a460f3e4e8e1c019daebdd88
Sven Stiels <52165058+5v3n-08@users.noreply.github.com>
2021-10-18 01:20:11 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Clock from "react-live-clock";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+interface IProps {}
+export const App: React.FC<IProps> = (
+  props: React.PropsWithChildren<IProps>
+) => {
+  const params = getQueryParams(window.location.search);
+  const color = params["color"] ? `#${params["color"]}` : "#040c23";
+  const font = params["font"] ? `#${params["font"]}` : "#fff";
+  const sizeTime = parseInt(params["sizeTime"] ?? "52");
+  const sizeDate = parseInt(params["sizeDate"] ?? "26");
+
+  console.log(params);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100vw", backgroundColor: color }}>
+      <div
+        className="d-flex justify-content-center align-items-center h-100"
+        style={{ flexDirection: "column" }}
+      >
+        <div style={{ color: font, fontSize: sizeTime }}>
+          <Clock format={"HH:mm"} ticking={true} />
+        </div>
+        <div style={{ color: font, fontSize: sizeDate }}>
+          <Clock format={"D.M.y"} ticking={true} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
+const getQueryParams = (qs: string) => {
+  qs = qs.split("+").join(" ");
+
+  const params: { [key: string]: string } = {};
+  let tokens: RegExpExecArray | null = null;
+  const re = /[?&]?([^=]+)=([^&]*)/g;
+
+  while ((tokens = re.exec(qs))) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+  }
+  return params;
+};
 export default App;
